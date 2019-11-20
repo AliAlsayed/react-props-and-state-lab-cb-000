7696620c677b294feed1b377568951bae9d8452b
Ali Alsayed <a_alsayed87@hotmail.com>
2019-11-20 08:30:20 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -59,7 +59,7 @@ class App extends React.Component {
             </div>
             <div className="twelve wide column">
               <PetBrowser pets={this.state.pets}
-               onAdoptPet={this.handleAdoption}/>
+               onAdoptPet={this.handleAdoption(id)}/>
             </div>
           </div>
         </div>
