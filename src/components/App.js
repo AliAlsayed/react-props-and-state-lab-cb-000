0b817e828214d65600537d3aec3c5e6286e9132e
Ali Alsayed <a_alsayed87@hotmail.com>
2019-11-20 08:02:37 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,36 @@ class App extends React.Component {
     }
   }
 
+  handleChangeType = (newType) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: newType
+      }
+    })
+  }
+
+  handleFindPets = () => {
+    if(this.state.filters.type === 'all'){
+      fetch('/api/pets')
+      .then(res => res.json())
+      .then(res => this.setState({ pets: res }))
+      .catch(() => this.setState({ hasErrors: true }));
+    } else {
+      fetch(`/api/pets?type=${this.state.filters.type}`)
+      .then(res => res.json())
+      .then(res => this.setState({ pets: res }))
+      .catch(() => this.setState({ hasErrors: true }));
+    }
+  }
+
+  handleAdoption = (id) => {
+    if(this.pets[id]){
+      this.pets[id].isAdopted = true
+      this.forceUpdate();
+    }
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +54,12 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleChangeType}
+              onFindPetsClick={this.handleFindPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets}
+               onAdoptPet={this.handleAdoption}/>
             </div>
           </div>
         </div>
